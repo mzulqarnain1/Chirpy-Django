4788aca3962c87d34235e08ad131ebf0097fbbc3
M. Zulqarnain <zulqarnain.mailbox@gmail.com>
2022-04-17 20:04:16 +0500

    Added coverage check in CI

---
+++ test/social/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from social.models import Post, Comment
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='test123')
+        self.post = Post.objects.create(body="This is a test post.", author=self.user)
+        self.comment = Comment.objects.create(body="This is a test comment.", author=self.user, post=self.post)
+
+    def test_animals_can_speak(self):
+        """Animals that can speak are correctly identified"""
+        post = Post.objects.get(author=self.user)
+        comment = Comment.objects.get(post=self.post)
+
+        self.assertEqual(str(post), "Post by test")
+        self.assertEqual(post.body, "This is a test post.")
+        self.assertEqual(comment.body, "This is a test comment.")
